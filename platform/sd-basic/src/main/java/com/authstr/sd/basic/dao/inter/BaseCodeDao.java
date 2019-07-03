9fed09684cbd9b21ca16dc02cf66a5c145bfb605
xiaole <wxl101801@163.com>
2019-07-03 21:46:36 +0800

    编码表

--- platform/sd-basic/src/main/java/com/authstr/sd/basic/dao/inter/BaseCodeDao.java
+++ platform/sd-basic/src/main/java/com/authstr/sd/basic/dao/inter/BaseCodeDao.java
@@ -1,24 +1,16 @@
 package com.authstr.sd.basic.dao.inter;
 
 import com.authstr.ff.utils.http.RequestPara;
-import com.authstr.ff.utils.page.ReturnPage;
 import com.authstr.ff.utils.page.QueryCommonPage;
+import com.authstr.ff.utils.page.ReturnPage;
 import com.authstr.ff.utils.web.dao.InterfaceDao;
 
 import java.util.List;
 import java.util.Map;
 
-public interface UserDao extends InterfaceDao{
+public interface BaseCodeDao extends InterfaceDao{
 
-	/**
-	 * 
-	 * @param query
-	 * @return
-	 * @time 2018年10月27日 下午5:59:24
-	 * @author authstr
-	 */
 	ReturnPage query(QueryCommonPage query, RequestPara para);
-
 
     List<Map> get(RequestPara para);
 }
